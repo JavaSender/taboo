afd8116a78b912392ea4cefa02ba9f723e50d0bb
java <mail3java3sender@gmail.com>
2024-06-26 00:06:40 +0200

    create repo

---
+++ src/main/java/pl/pcz/taboo/TabooApplication.java
@@ -0,0 +1,12 @@
+package pl.pcz.taboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabooApplication.class, args);
+	}
+}
